59f8e83605960b03a5a4585fe4e76e082e7cccb0
AlexGybou <Alexis@imac-de-alexis-1.home>
2015-09-17 22:36:53 +0200

    menu

--- template/main.php
+++ template/main.php
@@ -9,19 +9,6 @@
   		<img class="text-center logo" src="image/logo_small.png" />
   	</div>-->
 	<nav class="navbar navbar-grey" role="navigation">
-	  <div class="container-fluid">
-	    <!-- Brand and toggle get grouped for better mobile display -->
-	    <div class="navbar-header">
-	      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-	        <span class="sr-only">Toggle navigation</span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	      </button>
-	    </div>
-
-	    <!-- Collect the nav links, forms, and other content for toggling -->
-	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 	      <ul class="nav navbar-nav">
 	        <li class="pull-left"><a href="#">Blabla</a></li>
 			<img class="text-center logo" src="image/logo_small.png" />

cfdb6a46514a5c445bd5f3ec724897e0c726152b
BaptisteBriois <baptiste.briois@hotmail.fr>
2015-09-17 22:24:53 +0200

    coucou

--- template/_header.php
+++ template/_header.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>PBP</title>
+    <title>Mine</title>
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">

90f88f6be83818f2cecac4dfa28b362275a33e27
AlexGybou <Alexis@imac-de-alexis-1.home>
2015-09-17 22:40:04 +0200

    correctif

--- template/main.php
+++ template/main.php
@@ -1,11 +1,11 @@
 <body style="margin:0">
 <div class="container-fluid">
-	<div class="col-xs-10 col-xs-offset-1" id="header">
+	<!--<div class="col-xs-10 col-xs-offset-1" id="header">
   		<img class="logo" src="image/logo_small.png" class="center-block"/>
   	</div>
   	<div class="col-xs-1">
   	</div>
-  	<!--<div class="col-xs-12" id="header">
+  	<div class="col-xs-12" id="header">
   		<img class="text-center logo" src="image/logo_small.png" />
   	</div>-->
 	<nav class="navbar navbar-grey" role="navigation">
@@ -27,7 +27,7 @@
 		  	</div>
 			<canvas id="canvas" style="border: 5px solid black;"></canvas>
 	  	</div>
-<<<<<<< HEAD
+
 		<canvas id="canvas" width="800" height="600" style="border: 5px solid black;"></canvas>
   	</div>
   	<div class="col-xs-12 text-center">
@@ -45,7 +45,7 @@
 	  		<button type="button" id="who" class="btn btn-xl"><span class="glyphicon glyphicon-camera" aria-hidden="true"></span></button>
 	  	</div>
 		<canvas id="canvas2" width="800" height="600" style="border: 5px solid black;"></canvas>
-=======
+
     </div>
   	<div class="container">
 	  	<div class="col-xs-12">
@@ -57,7 +57,7 @@
 		  	</div>
 			<canvas id="canvas2" style="border: 5px solid black;"></canvas>
 	  	</div>
->>>>>>> origin/master
+
   	</div>
   	<button class="btn btn-default" type="submit"><a href="facebookconnect/logout.php">Me déconnecter</a></button>
 </div>

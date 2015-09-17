33735a6c255b8c55769808012a6c5874ef866dc1
Damien Duvernois <d.duvernois@student.isartdigital.com>
2015-09-17 22:32:23 +0200

    Ajout de fonctionnalité
    Gestion de base de donnée complète. Reste à ajouter des fonctionnalité
    esthétique.

--- template/sharedobjects.php
+++ template/sharedobjects.php
@@ -1,6 +1,8 @@
 <body>
 <div class="container-fluid">
-
+<div class="col-xs-10 col-xs-offset-1">
+	<a href="dashboard.php" class="btn btn-default btn-lg btn-block" role="button">Retour</a>
+</div>
 <?php
 foreach ($objects as $object) {
 	$date = substr($object['created_at'], 0, 10);

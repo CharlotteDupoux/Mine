10147526c472420fe6b35744b31bca3833fef690
AlexGybou <Alexis@imac-de-alexis-1.home>
2015-09-17 22:26:28 +0200

    Classement

--- template/main.php
+++ template/main.php
@@ -25,23 +25,27 @@
 	    </div><!-- /.navbar-collapse -->
 	  </div><!-- /.container-fluid -->
 	</nav>
-  	<div class="col-xs-12">
-	  	<button type="button" class="btn btn-lg btn-block">What?</button>
-	  	<video id="video" width="100%" autoplay></video>
-	  	<br>
-	  	<div class="text-center">
-	  		<button type="button" id="what" class="btn btn-xl"><span class="glyphicon glyphicon-camera" aria-hidden="true"></span></button>
+	<div class="container">
+	  	<div class="col-xs-12">
+		  	<button type="button" class="btn btn-lg btn-block">What?</button>
+		  	<video id="video" width="100%" autoplay></video>
+		  	<br>
+		  	<div class="text-center">
+		  		<button type="button" id="what" class="btn btn-xl"><span class="glyphicon glyphicon-camera" aria-hidden="true"></span></button>
+		  	</div>
+			<canvas id="canvas" style="border: 5px solid black;"></canvas>
 	  	</div>
-		<canvas id="canvas" style="border: 5px solid black;"></canvas>
-  	</div>
-  	<div class="col-xs-12">
-	  	<button type="button" class="btn btn-lg btn-block">Who?<br></button>
-	  	<video id="video2" width="100%" autoplay></video>
-	  	<br>
-	  	<div class="text-center">
-	  		<button type="button" id="who" class="btn btn-xl"><span class="glyphicon glyphicon-camera" aria-hidden="true"></span></button>
+    </div>
+  	<div class="container">
+	  	<div class="col-xs-12">
+		  	<button type="button" class="btn btn-lg btn-block">Who?<br></button>
+		  	<video id="video2" width="100%" autoplay></video>
+		  	<br>
+		  	<div class="text-center">
+		  		<button type="button" id="who" class="btn btn-xl"><span class="glyphicon glyphicon-camera" aria-hidden="true"></span></button>
+		  	</div>
+			<canvas id="canvas2" style="border: 5px solid black;"></canvas>
 	  	</div>
-		<canvas id="canvas2" style="border: 5px solid black;"></canvas>
   	</div>
 </div>
 	<button class="btn btn-default btn_deconexion" type="submit"><a href="facebookconnect/logout.php">Me déconnecter</a></button>

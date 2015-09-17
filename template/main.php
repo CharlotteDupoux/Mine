c3283984b90076d3cdbbb98ef3ea9002f6bea514
AlexGybou <Alexis@imac-de-alexis-1.home>
2015-09-17 22:17:56 +0200

    menu

--- template/main.php
+++ template/main.php
@@ -1,8 +1,30 @@
 <body style="margin:0">
 <div class="container-fluid">
-  	<div class="col-xs-12" id="header">
-  		<img class="logo" src="image/logo_small.png" />
-  	</div>
+  	<!--<div class="col-xs-12" id="header">
+  		<img class="text-center logo" src="image/logo_small.png" />
+  	</div>-->
+	<nav class="navbar navbar-grey" role="navigation">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	    </div>
+
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav">
+	        <li class="pull-left"><a href="#">Blabla</a></li>
+			<img class="text-center logo" src="image/logo_small.png" />
+	        <li class="social pull-right"><a href="#">Blabla</a></li>
+	      </ul>
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
   	<div class="col-xs-12">
 	  	<button type="button" class="btn btn-lg btn-block">What?</button>
 	  	<video id="video" width="100%" autoplay></video>

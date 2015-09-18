d56942519e919d0e29810809a7f85e30170179f0
vicator6 <Victor@MacBook-Pro.local>
2015-09-18 17:31:13 +0200

    UPDATE FINAL

--- template/_header.php
+++ template/_header.php
@@ -8,7 +8,9 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="model/functions.fn.js"></script>
+
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800' rel='stylesheet' type='text/css'> <script type="text/javascript" src="model/functions.fn.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/animate.css"/>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" href="css/accueil.css"/>
--- template/sharedobjects.php
+++ template/sharedobjects.php
@@ -1,36 +1,36 @@
 <body>
 <div class="container-fluid">
-<nav class="navbar navbar-default">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-	        <span class="sr-only">Toggle navigation</span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-      	</button>
-		<img class="logo-header center-block" src="image/logo_small.png">
-		<div class="col-xs-12">
-			<btn class="btn btn-danger btn-lg btn-block" role="button" id="suppresion">Suppresion</btn>
-			<btn class="btn btn-primary btn-lg" role="button" id="validate">OK</btn>
-		</div>
-    </div>
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li><a href="dashboard.php">Retour</a></li>
-        <li><a href="facebookconnect/logout.php">Se déconnecter</a></li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-    <?php if (isset($_SESSION['FBID'])) { ?>
-      <img src="https://graph.facebook.com/<?php echo $_SESSION['FBID'] ?>/picture" alt="<?php $_SESSION['FULLNAME'] ?>" style="position: absolute; top: 0; right: 5px">
-    <?php };?>
-</nav>
-<?php
-foreach ($objects as $object) {
-	$date = substr($object['created_at'], 0, 10);
-	$daterefine = str_replace('-', '/', $date);
-	echo '
+    <nav class="navbar navbar-default">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <img class="logo-header center-block" src="image/logo_small.png">
+
+                <btn class="btn" role="button" id="suppresion"><i class="fa fa-times fa-2x"></i></btn>
+                <btn class="btn btn-primary btn-lg" role="button" id="validate">OK</btn>
+
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="dashboard.php">Retour</a></li>
+                <li><a href="facebookconnect/logout.php">Se déconnecter</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+        <?php if (isset($_SESSION['FBID'])) { ?>
+            <img src="https://graph.facebook.com/<?php echo $_SESSION['FBID'] ?>/picture" alt="<?php $_SESSION['FULLNAME'] ?>" style="position: absolute; top: 0; right: 5px">
+        <?php };?>
+    </nav>
+    <?php
+    foreach ($objects as $object) {
+        $date = substr($object['created_at'], 0, 10);
+        $daterefine = str_replace('-', '/', $date);
+        echo '
 		<div class="row object-display">
 			<div class="col-xs-12">
 				<div class="col-xs-10 col-xs-offset-10 delete-object">
@@ -45,17 +45,40 @@ foreach ($objects as $object) {
 				</div>
 				<br>
 				<div class="col-xs-5">
-					<img src="image/'.$object['photoObject'].'" alt="objet" class="img-responsive">
+				<p class="what-ok">What?</p>
+					<img class="photoobjet" src="image/'.$object['photoObject'].'" alt="objet" class="img-responsive">
+
 				</div>
 				<div class="col-xs-5 col-xs-offset-1">
-					<img src="image/'.$object['photoPerson'].'" alt="objet" class="img-responsive">
+				<p class="what-ok">Who?</p>
+					<img class="photopersonne" src="image/'.$object['photoPerson'].'" alt="objet" class="img-responsive">
 				</div>
 				<br>
-				<p class="col-xs-12 text-center"><small>prêté le '.$daterefine.'</small></p>
+				<p class="cki">Mathieu Payta</p>
+				<p class="ckoi">Manette PS3</p>
+
+				<p class="col-xs-12 text-center"><small>Prêté le '.$daterefine.'</small></p>
+
+                <div class="col-xs-11"></div>
+
+                <div class="col-xs-1 ">
+                    <a href="" alt="Modifier">
+                    <i class="fa fa-pencil fa-2x" ></i>
+                    </a>
+                    <a href=""  alt="Alerter machin">
+                    <i class="fa fa-bell-o fa-2x animated tada infinite" ></i>
+                    </a>
+                </div>
+
+
 			</div>
+			<div class="separation"></div>
 		</div>
+
+
 	';
-}
-?>
+    }
+    ?>
 </div>
 </body>
+

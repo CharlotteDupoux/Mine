572dc7e9be5ffa5b8fd43daf85455d10b73212d5
Damien Duvernois <d.duvernois@student.isartdigital.com>
2015-09-18 15:08:03 +0200

    clean navbar

--- template/main.php
+++ template/main.php
@@ -1,5 +1,4 @@
 <body>
-<<<<<<< Updated upstream
 	<!--<div class="col-xs-10 col-xs-offset-1" id="header">
   		<img class="logo" src="image/logo_small.png" class="center-block"/>
   	</div>
@@ -31,7 +30,6 @@
       <img src="https://graph.facebook.com/<?php echo $_SESSION['FBID'] ?>/picture" alt="<?php $_SESSION['FULLNAME'] ?>" style="position: absolute; top: 0; right: 5px">
     <?php };?>
   </div><!-- /.container-fluid -->
-=======
 <!--<div class="col-xs-10 col-xs-offset-1" id="header">
       <img class="logo" src="image/logo_small.png" class="center-block"/>
   </div>
@@ -40,32 +38,6 @@
   <div class="col-xs-12" id="header">
       <img class="text-center logo" src="image/logo_small.png" />
   </div>-->
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed #animenu" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <div class="text-center"><img class="logo-header" src="image/logo_small.png"></li></div>
-        </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li><a href="delete.php">Objets prêtés </a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="facebookconnect/logout.php">Se déconnecter</a></li>
-            </ul>
-        </div><!-- /.navbar-collapse -->
-        <?php if (isset($_SESSION['FBID'])) { ?>
-            <img src="https://graph.facebook.com/<?php echo $_SESSION['FBID'] ?>/picture" alt="<?php $_SESSION['FULLNAME'] ?>" style="position: absolute; top: 0; right: 5px">
-        <?php };?>
-    </div><!-- /.container-fluid -->
->>>>>>> Stashed changes
-</nav>
 <div>
     <div class="what-display"></div>
     <div class="what-info">

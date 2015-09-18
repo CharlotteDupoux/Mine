34a0748a3a287347bb3f06653e92c6c552a11057
Gauthier Flichy <ga2015@flichy.biz>
2015-09-18 15:14:26 +0200

    update try + main

--- template/main.php
+++ template/main.php
@@ -66,6 +66,8 @@
     <form method="POST" action="try.php">
         <input type="hidden" name="what" value="" id="what-form">
         <input type="hidden" name="who" value="" id="who-form">
+        <input type="text" name="NameObject" value="What ?" id="NameObject">
+        <input type="text" name="NamePerson" value="Who ?" id="NamePerson">
         <input class="bouton" type="submit" name="deconnexion" value="Envoyez">
     </form>
 </div>
